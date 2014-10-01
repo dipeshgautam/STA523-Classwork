501a5a878e6721c474c7ac28855209ccdff09df3
Dipesh Gautam <digtm123@gmail.com>
2014-10-01 13:20:20 -0400

    changed a bit and created the folder for data

--- HW2/data_download_laquinta.r
+++ HW2/data_download_laquinta.r
@@ -1,8 +1,22 @@
 source("check_packages.R")
 check_packages(c("httr","XML","stringr","jsonlite","rgeos","maptools","stringr"))
 url= "http://www.lq.com/en/findandbook.html"
-d=as.vector(GET(url))
-a <- str_match_all(d, "push(.*)hotelDetails") 
+d=GET(url)
+s = content(d, as="text")
+a <- str_match_all(s, "push(.*countryDisplay: \"[a-zA-Z ]*\")") 
+file = paste0("data/", "laquinta.txt")
+write(s, file=file)
 
-ID <- unlist(str_match_all(d," id=\"g([0-9]*)\""))[seq(2,386,2)]
-  
+      
+{title: "La Quinta Inn & Suites St. George",
+ innNumber: "6024",
+ latitude:  "37.06301", longitude: "-113.58283",
+ imagePath: "/bin/lq-com/hotelSearchImage.6024.jpg",
+ isInnAndSuites: "true",
+ street: "91 East 2680 South",
+ street2: "",
+ city: "Saint George",
+ stateProv: "UT",
+ postalCode: "84790",
+ countryDisplay: "United States"
+}
