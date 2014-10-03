33c56d29436ee58980610e242198e1fa17af2a43
Heather Shapiro <hshapiro93@gmail.com>
2014-10-03 13:52:52 -0400

    added lq parsing

--- HW2/lq_data_download.r
+++ HW2/lq_data_download.r
@@ -4,7 +4,10 @@ check_packages(c("httr","XML","stringr","jsonlite","rgeos","maptools","stringr")
 dir.create("lq/", showWarnings = FALSE)
 
 url= "http://www.lq.com/en/findandbook.html"
-d=GET(url)
-s = content(GET(url), as="text")
-
+page = GET(url)
+s = content(page, as="text")
 write(s, file="lq/lq_list.txt")
+
+
+
+
