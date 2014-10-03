c2516dd16f83fb2c0967cf0f783aebc61085ae8d
Heather Shapiro <hshapiro93@gmail.com>
2014-10-03 13:52:52 -0400

    added lq parsing

--- HW2/lq_data_parse.r
+++ HW2/lq_data_parse.r
@@ -1,26 +1,44 @@
 source("check_packages.R")
-check_packages(c("httr","XML","stringr","jsonlite","rgeos","maptools","stringr"))
+check_packages(c("httr","XML","stringr","jsonlite","rgeos","maptools","stringr", "RJSONIO"))
 
 s = readLines("lq/laqinta_list.txt")
 
-name <- unlist(str_match_all(s, "title: \"([a-zA-Z0-9 &-/]*)\", "))[seq(2,1758,2)]
-innNumber <- unlist(str_match_all(s, "innNumber: \"([0-9]*)\", "))[seq(2,1758,2)]
-lat <- unlist(str_match_all(s, "\"([0-9.]*)\", longitude:"))[seq(2,1758,2)]
-long <- unlist(str_match_all(s, "longitude: \"(-[0-9.]*)"))[seq(2,1758,2)]
-street <- unlist(str_match_all(s, "street: \"([0-9a-zA-Z -.]*)\", "))[seq(2,1758,2)]
-city <- unlist(str_match_all(s, "city: \"([a-zA-Z]*)"))[seq(2,1758,2)]
-stateProv <- unlist(str_match_all(s, "stateProv: \"([a-zA-Z]*)"))[seq(2,1758,2)]
-postalCode <- unlist(str_match_all(s, "postalCode: \"([0-9A-Z-]*)"))[seq(2,1758,2)]
-country <- unlist(str_match_all(s, "countryDisplay: \"([a-zA-Z ]*)"))[seq(2,1758,2)]
+push = unlist(str_match_all(s, "hotelList.push?.+?}"))
+push=substring(push,16, length(push))
 
-lq_data <- data.frame(name=name, 
-                      innNumber=innNumber,
-                      lat=lat,
-                      long=long,
-                      street=street,
-                      city=city,
-                      postalCode=postalCode,
-                      stateProv=stateProv,
-                      country=country
-)
+hotels <- data.frame(Title=character(), InnNumber=character(), Latitude=character(),Longitude= character(),
+                     ImagePath=character(), SearchImage = character(), isInnAndSuites= character(),   
+                     street = character(), street2=character(), city = character(), stateProv=character(),
+                     postalCode = character(), countryDisplay=character(),
+                     stringsAsFactors=FALSE) 
 
+json_file = fromJSON(push[1])
+tmp =data.frame(Title=character(), InnNumber=character(), Latitude=character(),Longitude= character(),
+                ImagePath=character(), SearchImage = character(), isInnAndSuites= character(),   
+                street = character(), street2=character(), city = character(), stateProv=character(),
+                postalCode = character(), countryDisplay=character(),
+                stringsAsFactors=FALSE) 
+
+
+json_file = fromJSON(push[1])
+json_file <- lapply(json_file, function(x) {
+  x[sapply(x, is.null)] <- NA
+  unlist(x)
+})
+tmp = data.frame(do.call("cbind", json_file))
+tmp = rbind(tmp,data.frame(do.call("cbind", json_file)))
+
+for (i in 2:length(push)){
+  json_file <- fromJSON(push[i])
+  json_file <- lapply(json_file, function(x) {
+    x[sapply(x, is.null)] <- NA
+    unlist(x)
+  })
+  tmp = rbind(tmp,data.frame(do.call("cbind",json_file)))
+}
+
+hotels = tmp[!duplicated(tmp$nnNumbe),]
+colnames(hotels)= c("Title","InnNumber","Latitude","Longitude", "ImagePath", "isInnAndSuites","street","street2", "city", "stateProv",
+                    "postalCode", "countryDisplay")
+
+save(hotels,file = "lq/lq_list.Rdata")
