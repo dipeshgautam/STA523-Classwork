e313092b5907f31f6646ac151e7fc78cafba271a
Dipesh Gautam <digtm123@gmail.com>
2014-10-09 17:39:56 -0400

    Branching

--- HW2/lq_data_download2.R
+++ HW2/lq_data_download2.R
@@ -1,13 +1,15 @@
 source("check_packages.R")
 check_packages(c("httr","XML","stringr","jsonlite","rgeos","maptools","stringr"))
 
+hotels <- readRDS("lq/lq_URLS.Rdata")
 
-hotels =readRDS("lq/lq_URLS.Rdata")
-
-s= vector(length=nrow(hotels))
+s <- vector(length=nrow(hotels))
 for(j in 1:nrow(hotels)){
-  s[j]= RCurl::httpGET(hotels$urls[j])
-  Sys.sleep(rexp(1,1/5))
+  s[j] <- RCurl::httpGET(hotels$urls[j])
+  if (j%%50 == 0){
+    cat("Downloading LQ:",j,"\n")
+  }
+  Sys.sleep(runif(1))
 }
 
 saveRDS(s, file =  "lq/all_urls.Rdata")
