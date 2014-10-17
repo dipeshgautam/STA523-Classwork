49a94c445cc6530a3eee510d5b1cd9ad43649bb2
Dipesh Gautam <digtm123@gmail.com>
2014-10-17 15:52:30 -0400

    Fixed an issue with matching. Now we have 1.5 million matches.

--- HW3/tasks.R
+++ HW3/tasks.R
@@ -2,6 +2,7 @@ setwd("~/Team2/HW3") # Ensures check_packages.R is found.
 source("check_packages.R")
 check_packages(c("devtools", "stringr", "rgdal", "rgeos", "data.table"))
 install_github("hadley/dplyr")
+library(dplyr)
 
 base = '/home/vis/cr173/Sta523/data/parking' # Set to Dr. Rundel's Saxon directory containing NYParking data
 # Small Data, Use this for testing
@@ -28,7 +29,7 @@ addr = filter(park.full, Violation.Precinct <= 34) %>%
   transmute(Violation.Precinct = Violation.Precinct, addr = paste(House.Number, Street.Name)) %>%
   mutate(addr = tolower(addr))
 
-
+# addr1 <- addr
 rm(park)
 rm(park.full)
 
@@ -40,12 +41,29 @@ tax = cbind(data.frame(pt@coords), tolower(as.character(pl@data$Address)))
 names(tax)[3] = "addr"
 tax$addr = str_replace_all(tax$addr, "street", "st")
 tax$addr = str_replace_all(tax$addr, "avenue", "ave")
+tax$addr = str_replace_all(tax$addr, "lane", "ln")
+tax$addr = str_replace_all(tax$addr, "place", "pl")
+tax$addr = str_replace_all(tax$addr, "drive", "dr")
 tax$addr = str_replace_all(tax$addr, "east", "e")
 tax$addr = str_replace_all(tax$addr, "west", "w")
-addr$addr = str_replace_all(addr$addr, "[0-9]*th", "")
-addr$addr = str_replace_all(addr$addr, "1st", "1")
+tax$addr = str_replace_all(tax$addr, "south", "s")
+tax$addr = str_replace_all(tax$addr, "north", "n")
+
+
+addr$addr = str_replace_all(addr$addr, "th", "")
+addr$addr = str_replace_all(addr$addr, "[0-9]*1st", "1")
 addr$addr = str_replace_all(addr$addr, "2nd", "2")
 addr$addr = str_replace_all(addr$addr, "3rd", "3")
+addr$addr = str_replace_all(addr$addr, "east", "e")
+addr$addr = str_replace_all(addr$addr, "west", "w")
+addr$addr = str_replace_all(addr$addr, "south", "s")
+addr$addr = str_replace_all(addr$addr, "north", "n")
+addr$addr = str_replace_all(addr$addr, "street", "st")
+addr$addr = str_replace_all(addr$addr, "avenue", "ave")
+addr$addr = str_replace_all(addr$addr, "place", "pl")
+addr$addr = str_replace_all(addr$addr, "drive", "dr")
+addr$addr = str_replace_all(addr$addr, "lane", "ln")
+addr$addr = str_replace_all(addr$addr, "boradwya", "broadway")
 
 z = inner_join(tax, addr)
 #plot(z$y, z$x)
