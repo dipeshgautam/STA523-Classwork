a79c504d26390e6d15ce758b0c9008ade2e614ba
Andrew Wong <aw236@duke.edu>
2014-10-18 16:39:02 -0400

    Improved replacement code to increase number of matches. Commented code.

--- HW3/tasks.R
+++ HW3/tasks.R
@@ -1,69 +1,77 @@
 setwd("~/Team2/HW3") # Ensures check_packages.R is found.
-source("check_packages.R")
-check_packages(c("devtools", "stringr", "rgdal", "rgeos", "data.table"))
-install_github("hadley/dplyr")
-library(dplyr)
+source("check_packages.R") # Load check_packages function.
+check_packages(c("devtools", "stringr", "rgdal", "rgeos", "data.table", "maptools")) # Ensures listed packages are installed and load them. 
+install_github("hadley/dplyr") # Install github version of dplyr instead of CRAN version so inner.join() will not crash.
+library(dplyr) # 
 
-base = '/home/vis/cr173/Sta523/data/parking' # Set to Dr. Rundel's Saxon directory containing NYParking data
-# Small Data, Use this for testing
-# park = tbl_df(read.csv(paste0(base,"/NYParkingViolations_small.csv"), stringsAsFactors=FALSE)) # Create subset for proof-of-concept testing
-# addr = filter(park, Violation.Precinct <= 34) %>% 
-#   mutate(House.Number = str_trim(House.Number), Street.Name = str_trim(Street.Name)) %>%
-#   filter(House.Number != "" & Street.Name != "") %>%
-#   filter(str_detect(House.Number,"[0-9]+")) %>%
-#   transmute(Violation.Precinct = Violation.Precinct, addr = paste(House.Number, Street.Name)) %>%
-#   mutate(addr = tolower(addr))
-
-
-#Full Data
-park.full = tbl_df(fread(paste0(base,"/NYParkingViolations.csv"), stringsAsFactors=FALSE)) # Full data set using fread() for speed and convenience 
-park.full$'Summons Number'= as.numeric(park.full$'Summons Number')
-setnames(park.full, 'Violation Precinct', 'Violation.Precinct')
-setnames(park.full, 'House Number', 'House.Number')
-setnames(park.full, 'Street Name', 'Street.Name')
-
-addr = filter(park.full, Violation.Precinct <= 34) %>% 
+base <- '/home/vis/cr173/Sta523/data/parking' # Set to Dr. Rundel's Saxon directory containing NYParking data
+# Small data set for testing.
+park <- tbl_df(read.csv(paste0(base,"/NYParkingViolations_small.csv"), stringsAsFactors=FALSE)) # Create subset for proof-of-concept testing
+addr <- filter(park, Violation.Precinct <= 34) %>% 
   mutate(House.Number = str_trim(House.Number), Street.Name = str_trim(Street.Name)) %>%
   filter(House.Number != "" & Street.Name != "") %>%
   filter(str_detect(House.Number,"[0-9]+")) %>%
   transmute(Violation.Precinct = Violation.Precinct, addr = paste(House.Number, Street.Name)) %>%
   mutate(addr = tolower(addr))
 
-# addr1 <- addr
-rm(park)
-rm(park.full)
-
-pl = readOGR(paste0(base,"/pluto/Manhattan/"),"MNMapPLUTO") # Takes 3 minutes. Manhattan shapefile connecting property boundary polygons and addresses. 
-
-pt = gCentroid(pl,byid=TRUE) 
-
-tax = cbind(data.frame(pt@coords), tolower(as.character(pl@data$Address)))
-names(tax)[3] = "addr"
-tax$addr = str_replace_all(tax$addr, "street", "st")
-tax$addr = str_replace_all(tax$addr, "avenue", "ave")
-tax$addr = str_replace_all(tax$addr, "lane", "ln")
-tax$addr = str_replace_all(tax$addr, "place", "pl")
-tax$addr = str_replace_all(tax$addr, "drive", "dr")
-tax$addr = str_replace_all(tax$addr, "east", "e")
-tax$addr = str_replace_all(tax$addr, "west", "w")
-tax$addr = str_replace_all(tax$addr, "south", "s")
-tax$addr = str_replace_all(tax$addr, "north", "n")
+## Full data.
+park.full <- tbl_df(fread(paste0(base,"/NYParkingViolations.csv"), stringsAsFactors=FALSE)) # Full data set using fread() for speed and convenience 
+park.full$'Summons Number' <- as.numeric(park.full$'Summons Number') # Need to set as numeric if using fread() on data. 
+setnames(park.full, 'Violation Precinct', 'Violation.Precinct') # Rename variable.
+setnames(park.full, 'House Number', 'House.Number') 
+setnames(park.full, 'Street Name', 'Street.Name') 
 
 
-addr$addr = str_replace_all(addr$addr, "th", "")
-addr$addr = str_replace_all(addr$addr, "[0-9]*1st", "1")
-addr$addr = str_replace_all(addr$addr, "2nd", "2")
-addr$addr = str_replace_all(addr$addr, "3rd", "3")
-addr$addr = str_replace_all(addr$addr, "east", "e")
-addr$addr = str_replace_all(addr$addr, "west", "w")
-addr$addr = str_replace_all(addr$addr, "south", "s")
-addr$addr = str_replace_all(addr$addr, "north", "n")
-addr$addr = str_replace_all(addr$addr, "street", "st")
-addr$addr = str_replace_all(addr$addr, "avenue", "ave")
-addr$addr = str_replace_all(addr$addr, "place", "pl")
-addr$addr = str_replace_all(addr$addr, "drive", "dr")
-addr$addr = str_replace_all(addr$addr, "lane", "ln")
-addr$addr = str_replace_all(addr$addr, "boradwya", "broadway")
+addr <- filter(park.full, Violation.Precinct <= 34) %>% # Send subset: park's violation precincts less than or equal to precinct 34.
+  mutate(House.Number = str_trim(House.Number), Street.Name = str_trim(Street.Name)) %>% # Add new columns without white space.
+  filter(House.Number != "" & Street.Name != "") %>% # Subset excluding entries with missing house numbers and street names.
+  filter(str_detect(House.Number,"[0-9]+")) %>% # Subset only including house numbers with digits
+  transmute(Violation.Precinct = Violation.Precinct, addr = paste(House.Number, Street.Name)) %>% # 
+  mutate(addr = tolower(addr)) # 
 
-z = inner_join(tax, addr)
-#plot(z$y, z$x)
+# saveRDS(addr, ) # Save addr data frame locally.
+
+rm(park) # Improve speed by removing data set from memory.
+rm(park.full) # Improve speed by removing data set from memory.
+
+pl <- readOGR(paste0(base,"/pluto/Manhattan/"),"MNMapPLUTO") # Takes 2.5 minutes. Manhattan shapefile connecting property boundary polygons and addresses. 
+pt <- gCentroid(pl,byid = TRUE) # Store the centroid of the given geometry in pl data frame.
+tax <- cbind(data.frame(pt@coords), tolower(as.character(pl@data$Address))) # Add centroid coordinates and lowercase address of pl shapefile.
+names(tax)[3] <- "addr" # Rename third column of tax data frame to addr.
+# pl <- readShapeSpatial(paste0(base,"/pluto/Manhattan/"),"MNMapPLUTO")
+# tax <- data.frame(addr=pl$Address, precinct= pl$PolicePrct, coordinates(pl))
+
+## Rename observations to follow arbitrary naming conventions and improve matches between tax and addr data frames.
+tax$addr <- str_replace_all(tax$addr, "street", "st")
+tax$addr <- str_replace_all(tax$addr, "avenue", "ave")
+tax$addr <- str_replace_all(tax$addr, "lane", "ln")
+tax$addr <- str_replace_all(tax$addr, "place", "pl")
+tax$addr <- str_replace_all(tax$addr, "drive", "dr")
+tax$addr <- str_replace_all(tax$addr, "east", "e")
+tax$addr <- str_replace_all(tax$addr, "west", "w")
+tax$addr <- str_replace_all(tax$addr, "south", "s")
+tax$addr <- str_replace_all(tax$addr, "north", "n")
+
+addr$addr <- str_replace_all(addr$addr, "1st", "1")
+addr$addr <- str_replace_all(addr$addr, "2nd", "2")
+addr$addr <- str_replace_all(addr$addr, "3rd", "3")
+addr$addr <- str_replace_all(addr$addr, "5th", "5")
+addr$addr <- str_replace_all(addr$addr, "29th", "29")
+addr$addr <- str_replace_all(addr$addr, "163rd", "163")
+addr$addr <- str_replace_all(addr$addr, "east", "e")
+addr$addr <- str_replace_all(addr$addr, "west", "w")
+addr$addr <- str_replace_all(addr$addr, "south", "s")
+addr$addr <- str_replace_all(addr$addr, "north", "n")
+addr$addr <- str_replace_all(addr$addr, "street", "st")
+addr$addr <- str_replace_all(addr$addr, "avenue", "ave")
+addr$addr <- str_replace_all(addr$addr, "place", "pl") # Confirmed that "pl" is used for "place" rather than "plaza"
+addr$addr <- str_replace_all(addr$addr, "boulevard", "blvd")
+addr$addr <- str_replace_all(addr$addr, "drive", "dr")
+addr$addr <- str_replace_all(addr$addr, "lane", "ln")
+addr$addr <- str_replace_all(addr$addr, "bway", "broadway")
+addr$addr <- str_replace_all(addr$addr, "e.broadway", "e broadway")
+addr$addr <- str_replace_all(addr$addr, "boradwya", "broadway")
+addr$addr <- str_replace_all(addr$addr, "th", "")
+
+z <- inner_join(tax, addr) # Store matching addresses in "addr" and "tax" data frames.
+plot(z$y, z$x)
