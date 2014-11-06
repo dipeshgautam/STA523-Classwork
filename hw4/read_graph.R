694d39e05acc6544e1357800654962746017ceec
Dipesh Gautam <digtm123@gmail.com>
2014-11-06 18:09:16 -0500

    Completed write_graph. Both read and write are working for provided test cases. Might need to improve for a couple of general cases. There might be problems with names of vertices.

--- hw4/read_graph.R
+++ hw4/read_graph.R
@@ -1,11 +1,11 @@
 library(stringr)
-read_graph = function(file)
+read_graph <- function(file)
 {
   ## Check if the file exists.
   stopifnot(file.exists(file))
-  a = readLines(file)
+  a <- readLines(file)
   ## Create a list of approved structure for a line of dot file
-  valid.format = c("^[A-Z0-9]+ -> [A-Z0-9]+ \\[weight=[0-9e.+]+\\];", 
+  valid.format <- c("^[A-Z0-9]+ -> [A-Z0-9]+ \\[weight=[0-9e.+]+\\];", 
                    "^[A-Z0-9]+ -> [A-Z0-9]+;",
                    "^[A-Z0-9]+;",
                    "^\".+\" -> \".+\" \\[weight=[0-9e.+]+\\];",
@@ -20,12 +20,12 @@ read_graph = function(file)
   ## Check if each line is within given specification
   for (k in 1:length(a))
   {
-    valid = FALSE
+    valid <- FALSE
     for (l in 1:length(valid.format))
     {
       if (!is.na(str_match(a[k], valid.format[l]) ) )
       {
-        valid = TRUE
+        valid <- TRUE
         break
       }
     }
@@ -36,67 +36,67 @@ read_graph = function(file)
   }
 
   ## extract from and to vertices and weights as lists from the file
-  from = NULL
-  to = NULL
-  weights = NULL
+  from <- NULL
+  to <- NULL
+  weights <- NULL
   for(k in 1:length(a)){
     if(!is.na(str_match(a[k], "^.+ -> .+ \\[weight=[0-9e.+]*\\];") ))
     {
-      from[k] = unlist(str_match(a[k], "^(.+) ->"))[2]
-      to[k] = unlist(str_match(a[k], "-> (.+) \\["))[2]
-      weights[k] = unlist(str_match(a[k], "weight=([0-9e.+]*)\\]"))[2]
+      from[k] <- unlist(str_match(a[k], "^(.+) ->"))[2]
+      to[k] <- unlist(str_match(a[k], "-> (.+) \\["))[2]
+      weights[k] <- unlist(str_match(a[k], "weight=([0-9e.+]*)\\]"))[2]
     }
     else if (!is.na(str_match(a[k], "^.+ -> .+;") ))
     {
-      from[k] = unlist(str_match(a[k], "^(.+) ->"))[2]
-      to[k] = unlist(str_match(a[k], "-> (.+);"))[2]
-      weights[k] = 1
+      from[k] <- unlist(str_match(a[k], "^(.+) ->"))[2]
+      to[k] <- unlist(str_match(a[k], "-> (.+);"))[2]
+      weights[k] <- 1
     }
     else 
     {
-      from[k] = unlist(str_match(a[k], "^(.+);"))[2]
-      to[k] = ""
-      weights[k] = ""
+      from[k] <- unlist(str_match(a[k], "^(.+);"))[2]
+      to[k] <- ""
+      weights[k] <- ""
     }
   }
   
   ## If there is edge from vertex A to B but B doesn't have any edges, it's valid to not
   ## include it in the file and it's manually added below.
-  empty.to = rep("", length(setdiff(to[!to==""],from) ) ) # Check if there are unaccounted destination vertices, if there are get their number
-  from = c(from, setdiff(to[!to==""],from)) # Add the unaccounted edge as a vertix
-  to = c(to, empty.to)
+  empty.to <- rep("", length(setdiff(to[!to==""],from) ) ) # Check if there are unaccounted destination vertices, if there are get their number
+  from <- c(from, setdiff(to[!to==""],from)) # Add the unaccounted edge as a vertix
+  to <- c(to, empty.to)
     
   ## Create a list of unique starting vertices
-  unique.from = unique(from)
+  unique.from <- unique(from)
   
   ## Change the names of destination vertices into numeric indices
   for (j in 1:length(to))
   {
     if (!to[j]=="")
     {
-      to[j] = as.integer(which(unique.from==to[j]))
+      to[j] <- as.integer(which(unique.from==to[j]))
     }
   }
   
-  graph = list(list(list(), list() ))
+  graph <- list(list(list(), list() ))
   for (i in 1:length(unique.from))
   {
-    to.i = to[which(from==unique.from[i])]
-    weights.i = as.numeric(weights[which(from==unique.from[i])])
+    to.i <- to[which(from==unique.from[i])]
+    weights.i <- as.numeric(weights[which(from==unique.from[i])])
        
     if (length(to.i) == 1 && to.i== "")
     {
-      list.i = list(list(edges = integer(),
+      list.i <- list(list(edges = integer(),
                          weights = numeric()))
     }
     else
     {
-      list.i = list(list(edges = as.integer(c(to.i)),
+      list.i <- list(list(edges = as.integer(c(to.i)),
                          weights = c(weights.i)))
     }
     
-    graph[i] = list.i
-    names(graph)[i] = str_extract(unique.from[i], ignore.case("[A-Z0-9 ]+"))
+    graph[i] <- list.i
+    names(graph)[i] <- str_extract(unique.from[i], ignore.case("[A-Z0-9 ]+"))
     
   }
   return(graph)  
---
+++ hw4/write_graph.R
@@ -0,0 +1,87 @@
+library(stringr)
+write_graph <- function(graph, file)
+{
+  ## Check if the graph is valid
+  stopifnot(is_valid(graph))
+  ## Check if the directory exists, stop if it doesn't.
+  dir <- str_extract(file, '^.+\\/')
+  if (!is.na(dir) && !file.exists(dir))
+  {
+    stop("The directory you're trying to write to does not exist!")
+  }
+  ## Check if the file exists, stop if it does.
+  if (file.exists(file))
+  {
+    stop("The file already exists! Cannot overwrite!")
+  }
+  
+  file.create(file, showWarnings = TRUE)
+  
+  ## Check if the vertices are labelled, if not label them with their indices
+  for (k in 1:length(graph))
+  {
+    if (names(graph)[k] == ""|is.null(names(graph)[k])) 
+    {
+      names(graph)[k] <- as.character(k)
+    }
+    ## If the names are multiple words, put them in quotes
+    else if (!is.na(str_match(names(graph)[k], " ")))
+    {
+      names(graph)[k] <- paste0('"',names(graph)[k],'"' )
+    }
+    
+  }
+  
+  ## Check if the edges are weighted. Write into different formats accordingly.
+  weights <- NULL
+  for ( i in 1:length(graph))
+  {
+    weights <- c(weights, graph[[i]]$weights)
+  }
+  
+  ## If all the weights are equal to 1, write the lines without weights
+  if (length(unique(weights))==1 && unique(weights)==1)
+  {
+    for (i in 1:length(graph))
+    {
+      ## Check if the vertex has edges
+      if (length(graph[[i]]$edges) > 0)
+      {
+        for ( j in 1:length(graph[[i]]$edges) )
+        {
+          from <- names(graph)[i]      
+          to <- names(graph)[as.numeric(graph[[i]]$edges[j])]
+          line <- paste0(from, " -> ",to,";")
+          write(line, file = file,  append=TRUE)
+        }
+      }   
+    }
+  }
+  ## If the edges are weighted, include weight in the lines
+  else
+  {
+    for (i in 1:length(graph))
+    {
+      ## Check if the vertex has edges
+      if (length(graph[[i]]$edges) > 0)
+      {
+        for ( j in 1:length(graph[[i]]$edges) )
+        {
+          from <- names(graph)[i]      
+          to <- names(graph)[as.numeric(graph[[i]]$edges[j])]
+          weight <- as.numeric(graph[[i]]$weights[j])
+          line <- paste0(from, " -> ",to,
+                        " " ,"[weight=", weight,
+                        "];")
+          write(line, file=file,  append=TRUE)
+        }
+      }
+      else
+      {
+        from <- names(graph)[i]
+        line <- paste0(from,";")
+        write(line, file=file,  append=TRUE)
+      }
+    }
+  }
+}
