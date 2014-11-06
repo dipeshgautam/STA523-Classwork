284e6fe465f40db1a93a00e99a7d2915dd7aac97
Andrew Wong <aw236@duke.edu>
2014-11-06 12:09:10 -0500

    Added 2 more cases. One case left.

--- hw4/predicates.R
+++ hw4/predicates.R
@@ -1,4 +1,16 @@
 ## Testing
+graph1 = list(A = list(edges   = c(2L),
+                       weights = c(14)),
+              B = list(edges   = c(3L,4L),
+                       weights = c(23,13)),
+              D = list(edges   = c(1L),
+                       weights = c(5) ),
+              F = list(edges   = c(1L,5L),
+                       weights = c(43,33)),
+              N = list(edges   = c(1L,2L,4L),
+                       weights = c(33,-1,0)))
+
+
 graph2 = list(A = list(edges   = c(2L),
                        weights = c(14)),
               B = list(edges   = c(3L,4L),
@@ -9,36 +21,37 @@ graph2 = list(A = list(edges   = c(2L),
                        weights = c(43,33)),
               N = list(edges   = c(1L,2L,4L),
                        weights = c(33,22,11)))
+is_valid(graph1)
+test(graph1)
+
+g11 <- sapply(graph1, "[[", 1) # Grab first list element of n number of secondary lists (edges list). 
+g12 <- sapply(graph1, "[[", 2) # Grab first list element of n number of secondary lists (edges list). 
+g22 <- sapply(graph2, "[[", 1) # Grab first list element of n number of secondary lists (weights list). 
+g22 <- sapply(graph2, "[[", 2) # Grab first list element of n number of secondary lists (weights list). 
+test(g12)
+is_valid(graph1) # Should be false
+is_valid(graph2) # Should be true
+
+sapply(sapply(y, function(m) m <= 0), all, <=0) # Doesn't work.
+sapply(y, function(m) m <= 0) == c("TRUE") # Doesn't work.
+
+sapply(y, function(m) m <= 0) # Evaluates each element of each secondary list correctly.
+sapply(sapply(y, function(m) m <= 0), any) # Returns consensus value for each secondary list. 
+all(sapply(sapply(g12, function(m) m <= 0), any) == c("FALSE")) # Check if all weights lists have values <= 0. If TRUE, return TRUE.
+test(g12)
+
+is_valid(graph1)
+all(sapply(sapply(graph2, function(m) m <= 0), any) == c("FALSE"))
+test(y)
+test <- function(a) {
+  if (all(sapply(sapply(a, function(m) m <= 0), any) == c("FALSE"))) # Check if all weights lists have values <= 0.
+    return(TRUE)
+  return(FALSE)
+}
+
+is_valid(graph1)
+
 is_valid(graph2)
-class(a)
-c(class(a), sapply(a, class))
-
-c(names(a), sapply(a, names))
-c(names(graph2), sapply(graph2, names))
-c(sapply(graph2, "[[", 1)) # Extract the first element of each secondary-list.
-all( c(sapply(graph2, "[[", 1)) %in% 1:length(graph2))
-
-all( (sapply(graph2, "[[", 1)) %in% 1:5) # Doesn't work.
-all( (sapply(graph2, "[[", 1)) 1:5) # Doesn't work.
-
-
-z <- sapply(graph2, "[[", 1)
-sapply(sapply(graph2, "[[", 1), is.element, 1:5) == c("TRUE")
-
-sapply(graph2, "[[", 1) # Grab first of n number of secondary lists (edges list). 
-
-all(sapply(z, function(z) z %in% 1:5)) # Doesn't work.
-all(sapply(z, function(z) z %in% 1:5) == c("TRUE")) # Doesn't work.
-
-sapply(z, function(z) z %in% 1:5) # Works.
-all(sapply(z[2], is.element, 1:5) == c("TRUE")) # Works.
-all(sapply(z[1], is.element, 1:5) == c("TRUE")) # Substitute [1], 1:5
-all(vapply(z, is.element, 1:5) == c("TRUE")) # Substitute [1], 1:5
-all(sapply(z, is.element, 1:5) == c("TRUE")) # Substitute [1], 1:5
-?vapply
-
-all(z[[2]] %in% 1:5) # Works.
-
 
 is_valid = function(g) 
 {
@@ -49,10 +62,17 @@ is_valid = function(g)
       ## Check that each secondary list contains only edges and weights vectors 
       ## that are of the appropriate type.
       if (all( c(sapply(g, names)) == c("edges", "weights")))  
-        return(TRUE)
         ## Check that there are not any edges to non-existent vertices. 
-        ## Check that all weights are not less than or equal to 0. 
-        ## Check that every edge has a weight.  
+        if (all(sapply(sapply(sapply(g, "[[", 1), is.element, 1:length(g)), all, 1:length(g)) == c("TRUE"))) # Works!
+          ## Check that all weights are not less than or equal to 0. 
+          if (all(sapply(sapply(sapply(g, "[[", 2), function(m) m <= 0), any) == c("FALSE"))) # Check if all weights lists have values <= 0.
+            ## Check that every edge has a weight.
+            return(TRUE)
+          else
+            return(FALSE)
+        else
+          return(FALSE)
+            
       else
         # print("Secondary list does not only contain edge and weight vectors.") # Can't include print() --> error msg: unexpected 'else'.
         return(FALSE)
