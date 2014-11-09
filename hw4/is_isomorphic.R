cb30b5ad5ac6985c830a11e857254b9fbe12125c
Dipesh Gautam <digtm123@gmail.com>
2014-11-08 19:43:46 -0500

    Fixed is_isomorphic. It works for all the given test cases. Haven't looked at general cases. Also, fixed write_graph.R for issue with empty names that came up on one of the test cases given in min_span_tree.

--- hw4/is_isomorphic.R
+++ hw4/is_isomorphic.R
@@ -18,20 +18,20 @@ is_isomorphic <- function(g1,g2) {
   if(length(g1) != length(g2))
     return(FALSE)
   
+  match = matrix(0, nrow = length(g1), ncol = 1) ## mark whether edges are the same
   ##then compare each vertix and its related edges and weights
   for(i in 1:length(g1)){
     
-    flag = 0
-    match = matrix(0, nrow = length(g1), ncol = 1) ## mark whether edges are the same
-    
+    flag = 0   
     for(j in 1:length(g2)){
       if(names(g1[i]) == names(g2[j])) {
-        flag = 1 ## find out that vertex in graph2
-            
-        if(all(names(g1)[g1[[i]]$edges] == names(g2)[g2[[j]]$edges]))
+        flag = 1
+        ## find out that vertex in graph2
+        if (length(setdiff(names(g1)[g1[[i]]$edges], names(g2)[g2[[j]]$edges]))==0 &&
+              length(setdiff(names(g2)[g2[[j]]$edges], names(g1)[g1[[i]]$edges]))==0)
           match[i,1] = 1 ## the names of edges are the same
         else return(FALSE)      
-
+        
         o1 = order(g1[[i]]$edges)
         o2 = order(g2[[j]]$edges)
         
@@ -39,7 +39,6 @@ is_isomorphic <- function(g1,g2) {
           return(FALSE)
       }
     }
-        
   }
   
   if(flag != 1){
@@ -47,32 +46,5 @@ is_isomorphic <- function(g1,g2) {
   }
   
   if(all(match == 1))
-  return(TRUE)
+    return(TRUE)
 }
-
-#         ## compare related edges
-#         if(length(g1[[i]]$edges)!= length(g2[[j]]$edges))
-#             return(FALSE)
-#         
-#         all_match = 0
-#         
-#         for(k in 1:length(g1[[i]]$edges)){
-#             
-#             match = 0  
-#           
-#             for(p in 1:length(g2[[j]]$edges)){
-#               if(names(g1)[g1[[i]]$edges[k]] == names(g2)[g2[[j]]$edges[p]])
-#                 match = match + 1
-#             }
-#             
-#             if(match == length(g1[[i]]$edges) && match == length(g2[[j]]$edges))
-#               all_match = 1
-#         }
-#         
-#         if(all_match == 1)
-#           return(TRUE)
-#         else return(FALSE)
-
-
-#         
-#         if(g1 == g5 && g2 == g6) o2 = rev(order(g2[[j]]$edges))
