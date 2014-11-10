b90defb04368cff27ea4275a8bbc1db570df1e71
Andrew Wong <aw236@duke.edu>
2014-11-09 22:39:14 -0500

    Removed some code previously used for testing.

--- hw4/predicates.R
+++ hw4/predicates.R
@@ -23,7 +23,7 @@ is_valid = function(g)
                 ## Check for duplicate edges.
                 if (length(sapply(g, function(x) x[["edges"]])) == length(sapply(sapply(g, function(x) x[["edges"]]), unique)))
                     return(TRUE)
-                  else
+                  else {
                     print("Duplicate edges.")
                     return(FALSE)
                 }
@@ -93,24 +93,6 @@ is_undirected = function(g) {
     return(FALSE)
   }
 }
-
-length(sapply(g, function(x) x[["edges"]])) == length(unique(sapply(g, function(x) x[["edges"]])))
-length(sapply(g, function(x) x[["edges"]])) == length(unique(sapply(g, function(x) x[["edges"]])))
-
-length(sapply(g, function(x) x[["edges"]])) == length(sapply(sapply(g, function(x) x[["edges"]]), unique))
-
-g = list(A = list(edges  =2L,
-                   weights=1),
-          B = list(edges  =3L,
-                   weights=1),
-          C = list(edges  =5L,
-                   weights=1),
-          D = list(edges  =2L,
-                   weights=1),
-          E = list(edges  =c(6L,4L),
-                   weights=c(1,1)),
-          F = list(edges=integer(),
-                   weights=numeric()))  
 
 is_isomorphic = function(g1, g2)
 {
