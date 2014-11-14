e025d1c813e658339a90847781650729c7e006e2
Andrew Wong <aw236@duke.edu>
2014-11-14 14:23:40 -0500

    draft - before cleaning code

--- hw5/samplers.R
+++ hw5/samplers.R
@@ -4,54 +4,49 @@ reject <- function(n, dfunc, range, mc=FALSE) {
   # range = numeric vector defining the min & max range of the pdf (e.g.: range = c(0, 1) )
     # Used for the vector of quantiles: x <- seq(range[1], range[2], length = n))
   # mc = multicore
-  return(dfunc(seq(range[1], range[2], length = n)))
-  M <- 1 # Tuning parameter (scalar) that lifts uniform over function's pdf.
+  M = min(100,max(dfunc(seq(range[1], range[2], length = 1e6)))) # Find the absolute max for the function
+  #M <- 100 # Tuning parameter (scalar) that lifts uniform over function's pdf.
   samples.uniform <- NULL # Create NULL list to store accepted proposal values under function's pdf.
   for (i in 1:n) {
-    proposal <- runif(1, min = range[1], max = range[2]) # Generate 1 proposal value with [range] defining min and max.
+    proposal_x <- runif(1, min = range[1], max = range[2]) # Generate 1 proposal value for x within range.
     # Calculate the ratio of the densities: the density of the function, and 
     # the density of a uniform distribution.
-    density.ratio <- dfunc(proposal)/(M*dunif(proposal, min = range[1], max = range[2]))
-    if (runif(1) < density.ratio)
-      samples.uniform <- c(samples.uniform, proposal) # Add newly accepted proposal value.
+    
+    proposal_y = runif(1, min=0, max = M)
+    
+    if (dfunc(proposal_x) > proposal_y)
+      samples.uniform <- c(samples.uniform, proposal_x) # If u < f(x) / M*g(x), then accept x as realization of f(x). Else, reject x and sample again.
+    
+    #density.ratio <- dfunc(proposal)/(M*dunif(proposal, min = range[1], max = range[2])) # f(x) / M*g(x)
+    ## f(x) == target distribution
+    ## g(x) == instrumental distribution
+    ## Sample x from g(x) and u from U(0,1), the uniform distro over the unit interval.
+    #if (runif(1) < density.ratio)  # Check whether or not u < f(x) / M*g(x)
+    #  samples.uniform <- c(samples.uniform, proposal) # If u < f(x) / M*g(x), then accept x as realization of f(x). Else, reject x and sample again.
   }
-  range <- c(0,1)
-  proposal <- runif(1, min = range[1], max = range[2])
-  proposal
-  dbetann(proposal)
+  return(samples.uniform)
+  #range <- c(0,1)
+#   proposal <- runif(1, min = range[1], max = range[2])
+#   proposal
+#   dbetann(proposal)
   # x <- seq(range[1], range[2], length = n)
   # Generate distribution that covers
   # envelope.pdf <- runif(n, min = 0, max = 1) # Generate envelope distribution that encompasses unknown distribution over its support.
   # dfunc(x, mean = 0, sd = 1) 
 }
+
+range <- c(0, 1)
 proposal <- runif(1, min = range[1], max = range[2]) # Generate 1 proposal value with [range] defining min and max.
-test <- function(n) {
-  samples.uniform <- NULL
-  for (i in 1:n) {
-    proposal <- runif(1, min = 0, max = 10)
-    samples.uniform <- c(samples.uniform, dbetann(proposal))
-  }
-  return(samples.uniform)
-}
-hist(test(5000))
-hist(samples.uniform)
-samples.uniform
-dbetann = function(x)
-{
-  dbeta(x,0.9,0.9)
-}
-samp <- NULL
-for (i in 1:500) {
-  proposal <- runif(1, min = 0, max = 1)
-  samp <- c(samp, dbetann(proposal))
-}
-plot(samp)
-reject(10, dnorm, c(0,1)) # Works.
-reject(100, dnorm, c(0,1)) # Works.
-reject(1000, dnorm, c(0,1)) # Works.
-reject(5000, dnorm, c(0,1)) # Works.
-plot(reject(5000, dnorm, c(0,1))) # Works.
-hist(reject(5000, dnorm, c(0,1))) # Works.
+proposal
+dfunc <- dbetann
+density.ratio <- dfunc(proposal)/(dunif(proposal, min = range[1], max = range[2])) # f(x) / M*g(x)
+density.ratio
+runif(1) < density.ratio
+
+
+head(reject(100, dbetann, c(0,1) ))
+plot(reject(100, dbetann, c(0,1) ))
+## Colin: reject(100, dunif, c(0,1) )
 
 mh = function(n, dfunc, range, mc=FALSE)
 { ... }
