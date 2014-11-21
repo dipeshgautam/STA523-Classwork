a59e126802771463350d3a5a647ee02de4d22fc2
sl331 <sl331@duke.edu>
2014-11-21 15:26:30 -0500

    MULTICORE ADDED

--- hw5/Metropolissampling.R
+++ hw5/Metropolissampling.R
@@ -1,14 +1,17 @@
 ## draw a theta star value from proposal distribution which depends on previous one
 ## calculate the acceptance ratio r, if runif(1,0,1) < r, theta next = theta star; else theta next = theta.
 ## ensure we have enough sample size
+library(parallel)
 
-mh <- function(n, dfunc, range, mc=FALSE)
+mh <- function(n, dfunc, range, mc)
 { 
   # n = samples
   # dfunc = density function
   # range = numeric vector defining the min & max range of the pdf (e.g.: range = c(0, 1) )
   # Used for the vector of quantiles: x <- seq(range[1], range[2], length = n))
   # mc = multicore
+  
+  mhsampler <- function(n, dfunc, range, mc = FALSE){
   
   sample <- matrix(NA, nrow = n, ncol = 1) ## create a matrix to save the results
   num <- 0 ## used for calculating acceptance ratio, ratio should be within 25%-40% 
@@ -64,10 +67,18 @@ mh <- function(n, dfunc, range, mc=FALSE)
     sample[i,1] <- theta
     ratio <- num/(n - 1) 
     cat(ratio,'\n')
-  }
-      
-      
-     
-    return(sample)
+  }  
+  
+  return(sample)
+}  
+  
+  
+  if(mc == TRUE && n > 1000){
+    cores = 8
+      return(unlist(mclapply(1:cores, function(x) mhsampler(ceiling(n/cores), dfunc, range, w),
+                         mc.cores = cores) ) )
+  
+    }else{
+      mhsampler(n, dfunc, range, w)
+    }
 }
-
